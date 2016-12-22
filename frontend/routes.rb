72cd50987e9dd643af6912750d1dcf733c06372d
jackflaps <kevin@jackflaps.net>
2016-12-22 14:58:55 -0700

    item linker changes

--- frontend/routes.rb
+++ frontend/routes.rb
@@ -1,3 +1,5 @@
 ArchivesSpace::Application.routes.draw do
   match('/plugins/ao_mods/:id/download' => 'ao_mods#download', :via => [:get])
+  match('/plugins/item_linker/:id/create' => 'item_linker#create', :via => [:get])
+  match('/plugins/item_linker/:id/update' => 'item_linker#update', :via => [:get])
 end
