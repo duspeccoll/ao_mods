0daafffe5aa748496913228a6a0b06c291e12553
jackflaps <kevin@jackflaps.net>
2017-03-14 21:44:59 -0600

    went back to batch downloading MODS from a list

--- frontend/controllers/ao_mods_controller.rb
+++ frontend/controllers/ao_mods_controller.rb
@@ -15,12 +15,13 @@ class AoModsController < ExportsController
   end
 
   def batch
-    batch_download(params['mods_resource']['ref'])
+    batch_download(params['mods_downloads'])
   end
 
   private
 
-  def get_mods(url)
+  def get_mods(uri)
+    url = URI("#{JSONModel::HTTP.backend_url}#{uri}")
     req = Net::HTTP::Get.new(url.request_uri)
     req['X-ArchivesSpace-Session'] = Thread.current[:backend_session]
     resp = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
@@ -28,30 +29,44 @@ class AoModsController < ExportsController
   	return mods
 	end
 
-	def process_tree(obj, zos, path)
-		url = "/repositories/#{session[:repo_id]}/archival_objects/mods/#{obj['id']}.xml"
-		if obj['level'] == "item"
-      filename = obj['component_id'] ? "#{obj['component_id']}.xml" : "#{obj['id']}.xml"
-			mods = get_mods(URI("#{JSONModel::HTTP.backend_url}#{url}"))
-      zos.put_next_entry "#{path}/#{filename}"
-      zos.puts mods
-		end
-		obj['children'].each do |child|
-			process_tree(child, zos, path)
-		end
-	end
+  def search_item(item)
+    search_data = Search.all(session[:repo_id],{
+      'q' => item,
+      'filter_term[]' => {'primary_type' => "archival_object"}.to_json
+    })
 
-	def batch_download(resource)
-    id = JSONModel::HTTP.get_json("#{resource}")['id_0'].downcase!
-    tree = JSONModel::HTTP.get_json("#{resource}/tree")
-    path = "#{id}_mods_download"
-    output = Zip::OutputStream.write_buffer do |zos|
-			tree['children'].each do |child|
-				process_tree(child, zos, path)
-			end
-		end
-		output.rewind
-    send_data output.read, :filename => "#{path}.zip", :type => "application/zip"
+    if search_data.results?
+      search_data['results'].each do |result|
+        obj = JSON.parse(result['json'])
+        return obj['uri'] if obj['component_id'] == item
+      end
+    end
+  end
+
+	def batch_download(downloads)
+    items = downloads.split(/\r\n/)
+    uris = []
+
+    items.each do |item|
+      uri = search_item(item)
+      uris.push({'item' => item, 'uri' => uri}) unless uri.nil?
+    end
+
+    if uris.empty?
+      flash[:error] = "Couldn't find any of the IDs provided!"
+      redirect_to :controller => :ao_mods, :action => :index
+    else
+      output = Zip::OutputStream.write_buffer do |zos|
+        uris.each do |uri|
+          uri['uri'].gsub!(/objects\//,'objects/mods/')
+          mods = get_mods("#{uri['uri']}.xml")
+          zos.put_next_entry "#{uri['item']}.xml"
+          zos.puts mods
+        end
+      end
+      output.rewind
+      send_data output.read, :filename => "batch_mods_download.zip", :type => "application/zip"
+    end
 	end
 
 end
